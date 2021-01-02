9fac27c551e6c763035d49f34534d0fff06c20d0
Alex Racinski <aracinski2@gmail.com>
2021-01-02 10:57:29 -0600

    add login page

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import axios from "axios";
 axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
+var jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
+}
+
 Vue.config.productionTip = false;
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue';
 import Signup from '../views/Signup.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: Signup
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   },
 ]
 
