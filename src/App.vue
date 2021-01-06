bacad2bbbf4e1a869ae5c7a916d1ac7d930ec6b2
Alex Racinski <aracinski2@gmail.com>
2021-01-05 20:57:27 -0600

    add different liquors to home page

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/about">About</router-link> | -->
       <router-link to="/signup">Signup</router-link> |
       <router-link to="/login">Login</router-link> |
       <a href="/logout">Logout</a>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,26 @@
     <router-link v-bind:to="{path: `/recipes?search=category&category=Vodka`}">
     <h1>Vodka</h1>
     </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Whiskey`}">
+    <h1>Whiskey</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Rum`}">
+    <h1>Rum</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Liquor`}">
+    <h1>Liqueur</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Wine`}">
+    <h1>Wine</h1>
+    </router-link>
+    
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Tequila`}">
+    <h1>Tequila</h1>
+    </router-link>
   </div>
 </template>
 
