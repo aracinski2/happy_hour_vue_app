df499a908a9a6a95e91ee4af5ad85902eea2329d
Alex Racinski <aracinski2@gmail.com>
2021-01-08 15:39:36 -0600

    add message, change h tags

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,54 +1,55 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h2>Click on a category of alcohol to get it's recipes</h2>
     <router-link v-bind:to="{path: `/recipes?search=category&category=Vodka`}">
       <!-- <img src="@/assets/vodka.jpg" contain/> -->
-      <h1>Vodka</h1>
+      <h2>Vodka</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Gin`}">
       <!-- <img src="@/assets/vodka.jpg" contain/> -->
-      <h1>Gin</h1>
+      <h2>Gin</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Whiskey`}">
       <!-- <img src="@/assets/whiskey.jpg"/> -->
-      <h1>Whiskey</h1>
+      <h2>Whiskey</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Rum`}">
       <!-- <img src="@/assets/rum.jpg"/> -->
-      <h1>Rum</h1>
+      <h2>Rum</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Tequila`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Tequila</h1>
+      <h2>Tequila</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Brandy`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Brandy</h1>
+      <h2>Brandy</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Liqueur`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Liqueur</h1>
+      <h2>Liqueur</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Liquor`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Schnapps</h1>
+      <h2>Schnapps</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Beer`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Beer</h1>
+      <h2>Beer</h2>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Wine`}">
       <!-- <img src="@/assets/vodka.jpg"/> -->
-      <h1>Wine</h1>
+      <h2>Wine</h2>
     </router-link>
     
   </div>
