daa41c6f148424e37a8102e0912aab9391f5d618
Alex Racinski <aracinski2@gmail.com>
2021-01-08 14:40:53 -0600

    add more categories to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,55 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <router-link v-bind:to="{path: `/recipes?search=category&category=Vodka`}">
-    <h1>Vodka</h1>
+      <!-- <img src="@/assets/vodka.jpg" contain/> -->
+      <h1>Vodka</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Gin`}">
+      <!-- <img src="@/assets/vodka.jpg" contain/> -->
+      <h1>Gin</h1>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Whiskey`}">
-    <h1>Whiskey</h1>
+      <!-- <img src="@/assets/whiskey.jpg"/> -->
+      <h1>Whiskey</h1>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Rum`}">
-    <h1>Rum</h1>
+      <!-- <img src="@/assets/rum.jpg"/> -->
+      <h1>Rum</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Tequila`}">
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Tequila</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Brandy`}">
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Brandy</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Liqueur`}">
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Liqueur</h1>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Liquor`}">
-    <h1>Liqueur</h1>
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Schnapps</h1>
+    </router-link>
+
+    <router-link v-bind:to="{path: `/recipes?search=category&category=Beer`}">
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Beer</h1>
     </router-link>
 
     <router-link v-bind:to="{path: `/recipes?search=category&category=Wine`}">
-    <h1>Wine</h1>
+      <!-- <img src="@/assets/vodka.jpg"/> -->
+      <h1>Wine</h1>
     </router-link>
     
-    <router-link v-bind:to="{path: `/recipes?search=category&category=Tequila`}">
-    <h1>Tequila</h1>
-    </router-link>
   </div>
 </template>
 
